8291f9eaa461e23a0b523d3791b62303870a254f
Sufiandy Elmy <elmysufiandy09@gmail.com>
2020-09-04 11:21:45 +0700

    Initial Commit

---
+++ LearningCoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningCoreML
+//
+//  Created by Sufiandy Elmy on 04/09/20.
+//  Copyright © 2020 Sufiandy Elmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
